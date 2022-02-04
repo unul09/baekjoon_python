569359ca61cf3f7dff8713055cd5934e293d26b3
unul0912 <89674578+unul0912@users.noreply.github.com>
2022-02-05 02:12:45 +0900

    1946 시간초과

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
-# 13305
-# 이번 도착지에서의 기름이 더 싸면 그걸로 교체, 아닐시 유지
-# 기름을 일정량 사서 소진하는 식으로 사고하면 안됨. 더 적은 기름값을 선택해서 운용한다고 사고하기
+# 1946
+# 서류1등보다 면접등수가 같거나높은놈들 + 면접1등보다 서류등수가 같거나높은놈들 -> 둘의 교집합
+import sys
 
-N = int(input())
-dis = list(map(int, input().split()))
-price = list(map(int, input().split()))
+T = int(input())
+for i in range(T):
+    data = []
+    N = int(input())
+    for j in range(N):
+        data.append(list(map(int, sys.stdin.readline().split())))
 
-nowPrice = price[0]
-total = 0
+    temp1 = []
+    temp2 = []
+    data.sort(key=lambda x: x[1])
+    for j in range(len(data)):
+        if data[j][0] == 1:
+            temp1 = data[:j+1]
 
-for i in range(N-1):
-    total += nowPrice * dis[i]
-    if nowPrice > price[i+1]:
-        nowPrice = price[i+1]
+    data.sort()
+    for j in range(len(data)):
+        if data[j][1] == 1:
+            temp2 = data[:j+1]
 
-print(total)
+    for k in range(len(temp2)):
+        temp1.append(temp2[k])
+    temp1.sort()
+    cnt = 0
+    flag = 1
+    for k in range(1, len(temp1)):
+        if temp1[k][0] == flag:
+            cnt += 1
+        else:
+            flag = temp1[k][0]
+
+    print(cnt)
 
 
 
