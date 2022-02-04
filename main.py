3e639d895549d8907ac7a06c1fdb0740fa0209fb
unul0912 <89674578+unul0912@users.noreply.github.com>
2022-02-04 23:35:51 +0900

    5585

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-# 1541 개선ver
-"""
-식이 주어진다. 식은 ‘0’~‘9’, ‘+’, 그리고 ‘-’만으로 이루어져 있음
+# 5585
 
-마이너스 기호 만나기 전, 후로 나누어
-전의 수는 더하기로 처리
-후의 수는 빼기로 처리
-"""
-arr = input().split('-')
-s = 0
-for i in arr[0].split('+'):
-    s += int(i)
-for i in arr[1:]:
-    for j in i.split('+'):
-        s -= int(j)
-print(s)
+M = 1000 - int(input())
+coin = [500, 100, 50, 10, 5, 1]
+index = 0
+total = 0
+while M != 0:
+    if M >= coin[index]:
+        total += int(M / coin[index])
+        M = M % coin[index]
 
+    index += 1
 
-
+print(total)
