ee033d59d6f5fc1b42e5400875df5469d6380ceb
unul0912 <89674578+unul0912@users.noreply.github.com>
2022-02-05 00:33:16 +0900

    10610

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-# 10162
+# 10610
+# 30의 배수 = 3의배수이면서 10의배수 = (각자릿수총합%3 ==0) and (일의자리수 0)
 
-T = int(input())
+N = input()
+nums = []
+for i in range(len(N)):
+    nums.append(int(N[i]))
 
-if T % 10 != 0:
+nums.sort(reverse=True)
+
+if (sum(nums) % 3 != 0) or (nums[len(nums) - 1] != 0):
     print(-1)
     exit()
 
-print(int(T / 300), end=' ')
-T = T % 300
-print(int(T / 60), end=' ')
-T = T % 60
-print(int(T / 10))
-
-
+for num in nums:
+    print(num, end='')
