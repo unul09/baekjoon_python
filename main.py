617a40e1c3cbe9c79e69c6eaebd335c39b60ee36
unul0912 <89674578+unul0912@users.noreply.github.com>
2022-02-05 07:29:12 +0900

    1715 (우선순위큐. 나중에다시)

--- main.py
+++ main.py
@@ -1,33 +1,21 @@
-# 1339
+# 1715
 '''
-유의사항 + 해결방안
-ABC + BCD
-100A + 110B + 10C + D
-
-처음에는 숫자.문자 매핑해서 하나하나 할당해주려 했는데
-그럴 시에 위에 경우에 잘못된 값을 답으로 내놓게 됨
-그러므로 그냥 수학적으로.!!! 접근하면.. 되는 것
+자료구조 종류, 각각 동작원리와 시간복잡도 공부할것
 '''
-import math
-import operator
+import heapq # 우선순위 큐
 
 N = int(input())
 
-coef = {}
-for _ in range(N):
-    word = input()
-    for i in range(len(word)):
-        if not (word[i] in coef):
-            coef[word[i]] = 0
-        coef[word[i]] += int(math.pow(10, len(word)-1 - i))
+cardList = list(int(input()) for _ in range(N))
+heapq.heapify(cardList)
+result=0
 
-coef_new = sorted(coef.items(), key=operator.itemgetter(1), reverse=True)
 
-sum = 0
-val = 9
-for i in coef_new:
-    sum += i[1] * val
-    val -= 1
+while len(cardList) != 1:
+    num1 = heapq.heappop(cardList)
+    num2 = heapq.heappop(cardList)
+    Sum = num1 + num2
+    result += Sum
+    heapq.heappush(cardList,Sum)
 
-print(sum)
-
+print(result)
