7897bfe002d40c96efe9e4b459bb15c4e14ef1f8
unul0912 <89674578+unul0912@users.noreply.github.com>
2022-02-04 23:07:00 +0900

    1541

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
-# 1931 개선ver
+# 1541
+"""
+식이 주어진다. 식은 ‘0’~‘9’, ‘+’, 그리고 ‘-’만으로 이루어져 있음
 
-'''
-문제해결 설명
-데이터를 끝나는 시간 기준 내림차순, 끝나는 시간 같을경우에는 그 안에서 시작시간 내림차순 정렬
-첫번째 데이터의 끝나는 시간 둔상태에서 리스트 한번 쫙~ 훝으며
-적합한 회의라면(시작시간이 end time보다 크거나같다면) 회의수 카운트증가 + end time 갱신
-'''
+"""
+import re
+exp = input()
 
-'''
-반복문으로 여러줄 입력받는 상황에서는 
-반드시 sys.stdin.readline()을 사용해야 시간초과가 발생하지 않습니다.
-'''
-import sys
-input = sys.stdin.readline
+numbers = re.findall(r'\d+', exp)
+chars = re.sub(r'[0-9]+', '', exp)
 
-n = int(input())
-data = []
-for _ in range(n): # i 할당없이 단순 n번반복!
-    data.append(list(map(int,input().split()))) #list로 만들어서 넣어주기!
+ans = int(numbers[0])
 
-data.sort(key=lambda x: (x[1], x[0]))
+if len(numbers) == 1:
+    print(ans)
+    exit()
 
-end_t = data[0][1]
-cnt = 1
+# 마이너스가 하나라도 나오는 순간부터 그 위는 다 마이너스로 처리 가능
+minus_Flag = False
+for i in range(len(chars)):
+    if chars[i] == '-':
+        minus_Flag = True
+
+    if minus_Flag:
+        ans -= int(numbers[i+1])
+    else:
+        ans += int(numbers[i+1])
 
 
-#리스트 변형 없이 그저 한번 훝어보기!!!
-for i in range(1, n):  #두번째놈부터 볼거임
-    if data[i][0] >= end_t:
-        cnt += 1
-        end_t = data[i][1]
+print(ans)
 
-print(cnt)
+
+
+
