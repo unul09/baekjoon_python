09e95fb4584b6f0d937866e3842b682372915252
unul0912 <89674578+unul0912@users.noreply.github.com>
2022-02-05 03:01:06 +0900

    1946 개선ver

--- backup.py
+++ backup.py
@@ -1,26 +1,3 @@
-while True:
-    clock = meeting[0][1]
-    total += 1
-    if_flag = False
-    for i in range(len(meeting)):
-        if meeting[i][0] >= clock:
-            meeting = meeting[i-1:]
-            if_flag = True
-            break
-
-    if not if_flag:
-        break
-
-
-    '''
-    # meeting배열에서 현재시각(clock)보다 이른 놈들 삭제
-    temp = []
-    for i in range(len(meeting)):
-        if meeting[i][0] >= clock:
-            temp.append(meeting[i])
-
-    if len(temp) == 0:
-        break
-    meeting = temp
-    '''
-
+arr = [10, 4, 5, 2, 24, 1, 9, 12]
+result = list(filter(lambda x: x < 10, arr))
+print(result)
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # 1946
-# 서류1등보다 면접등수가 같거나높은놈들 + 면접1등보다 서류등수가 같거나높은놈들 -> 둘의 교집합
+# 서류1등보다 면접등수가 같거나높은놈들 + 면접1등보다 서류등수가 같거나높은놈들 -> 둘의 교집합 XXXX
+# 위 방법으로 몇번 시도해봤는데 안됨.
+# 서류순으로 오름차순정렬, latest 합격자에 대하여
+
+#https://suhwanc.tistory.com/118 이새끼 미친새끼임... 사고의 흐름이 매우 체계적..!
 import sys
 
 T = int(input())
@@ -9,28 +13,17 @@ for i in range(T):
     for j in range(N):
         data.append(list(map(int, sys.stdin.readline().split())))
 
-    temp1 = []
-    temp2 = []
-    data.sort(key=lambda x: x[1])
-    for j in range(len(data)):
-        if data[j][0] == 1:
-            temp1 = data[:j+1]
+    if len(data) == 1:
+        print(1)
+        continue
 
     data.sort()
-    for j in range(len(data)):
-        if data[j][1] == 1:
-            temp2 = data[:j+1]
-
-    for k in range(len(temp2)):
-        temp1.append(temp2[k])
-    temp1.sort()
-    cnt = 0
-    flag = 1
-    for k in range(1, len(temp1)):
-        if temp1[k][0] == flag:
+    cnt = 1
+    latestOk = data[0][1]
+    for j in range(1, len(data)):
+        if data[j][1] < latestOk:
             cnt += 1
-        else:
-            flag = temp1[k][0]
+            latestOk = data[j][1]
 
     print(cnt)
 
