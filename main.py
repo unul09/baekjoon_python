1c09db48a96cd9cd92c848995a3813e5c894b793
unul0912 <89674578+unul0912@users.noreply.github.com>
2022-02-08 06:35:41 +0900

    2864

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
-# 1049
+# 2864
 
-# (필요개수/6 올림) X 세트최저가,
-# (필요개수/6 내림) X 세트최저가 + 나머지개수 X 단일최저가,
-# 필요개수 X 단일최저가 비교, 작은값 출력
+A, B = input().split()
 
-N, M = map(int, input().split())
-price = [list(map(int, input().split())) for _ in range(M)]
+A = A.replace("6", "5")
+B = B.replace("6", "5")
 
-setmin = price[0][0]
-for i in range(M):
-    if price[i][0] < setmin:
-        setmin = price[i][0]
+print(int(A) + int(B), end=' ')
 
-onemin = price[0][1]
-for i in range(M):
-    if price[i][1] < onemin:
-        onemin = price[i][1]
+A = A.replace("5", "6")
+B = B.replace("5", "6")
 
-import math
-a = math.ceil(N / 6) * setmin
-b = math.floor(N / 6) * setmin + (N % 6) * onemin
-c = onemin * N
-
-print(min(a, b, c))
-
-
-
-
+print(int(A) + int(B))
