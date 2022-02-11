efb31c5d5f090729de1e145251cba1850348c7e1
unul0912 <89674578+unul0912@users.noreply.github.com>
2022-02-12 05:37:14 +0900

    2437

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
-# 1080
-# 60퍼센트까지 가다가 틀린다!!
-# 문제 조건 제대로 해석할것. 3x3 미만크기 행렬에 대해 -1출력하라는 내용은 그 어디에도 없었음.
+# 2437
+# n-1번까지의 합 < n번째수 일때, 측정할수 없는 최소값은 (n-1번까지의 합 + 1)
+import sys
+N = int(sys.stdin.readline())
+scales = list(map(int, sys.stdin.readline().split()))
 
-N, M = map(int, input().split())
-A = [list(input()) for _ in range(N)]
-B = [list(input()) for _ in range(N)]
+scales.sort()
 
+# 첫번째 값이 1이 아닐경우 바로 나가리
+if scales[0] != 1:
+    print(1)
+    exit()
 
-cnt = 0
+sum_scales = 0
+for i in range(N-1):
+    sum_scales += scales[i] # 합계 구하기
+    if sum_scales + 1 < scales[i+1]: # 합계+1 보다 다음수가 더 크다면, 그 사이 갭이 있다는 말
+        print(sum_scales + 1)
+        exit()
 
-# 우측, 하단의 각 2줄은 딸려오는 놈들. 얘네 빼고 하나하나 비교해주며, 불일치할시 행렬뒤집기 실시
-for i in range(N - 2):
-    for j in range(M - 2):
-        if A[i][j] != B[i][j]:
-            cnt += 1
-            for ii in range(3):
-                for jj in range(3):
-                    A[i + ii][j + jj] = '1' if A[i + ii][j + jj] == '0' else '0'
-
-
-for i in range(N):
-    for j in range(M):
-        if A[i][j] != B[i][j]:
-            print(-1)
-            exit()
-
-print(cnt)
+sum_scales += scales[N-1] + 1 # 중간 갭 없이 연속적으로 모든 수가 나타날 경우, 수열 총합 +1 리턴
+print(sum_scales)
