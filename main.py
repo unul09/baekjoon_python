be3e2f28463175218262705e7e91a99d1505f658
unul0912 <89674578+unul0912@users.noreply.github.com>
2022-02-12 06:08:02 +0900

    1543

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-# 1449
-# 테이프 길이만큼의
-import sys
-N, L = map(int, sys.stdin.readline().split())
-leak = list(map(int, sys.stdin.readline().split()))
-leak.sort()
+# 1543
 
-temp = leak[0]
-cnt = 1 # 물새는곳은 무조건 1개이상이므로 처음부터 테이프 할당
+text = input()
+find = input()
 
-for i in range(1, N):
-    if leak[i] - temp < L:
-        continue
-    else:
-        temp = leak[i] # 새로운 테이프
-        cnt += 1
-
-print(cnt)
+print(len(text.split(find))-1)
