f47525fdf4579e556f97f936025d2b3d5f99a7c5
unul0912 <89674578+unul0912@users.noreply.github.com>
2022-02-12 04:32:03 +0900

    1080

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-# 2864
+# 1080
+# 60퍼센트까지 가다가 틀린다!!
+# 문제 조건 제대로 해석할것. 3x3 미만크기 행렬에 대해 -1출력하라는 내용은 그 어디에도 없었음.
 
-A, B = input().split()
+N, M = map(int, input().split())
+A = [list(input()) for _ in range(N)]
+B = [list(input()) for _ in range(N)]
 
-A = A.replace("6", "5")
-B = B.replace("6", "5")
 
-print(int(A) + int(B), end=' ')
+cnt = 0
 
-A = A.replace("5", "6")
-B = B.replace("5", "6")
+# 우측, 하단의 각 2줄은 딸려오는 놈들. 얘네 빼고 하나하나 비교해주며, 불일치할시 행렬뒤집기 실시
+for i in range(N - 2):
+    for j in range(M - 2):
+        if A[i][j] != B[i][j]:
+            cnt += 1
+            for ii in range(3):
+                for jj in range(3):
+                    A[i + ii][j + jj] = '1' if A[i + ii][j + jj] == '0' else '0'
 
-print(int(A) + int(B))
+
+for i in range(N):
+    for j in range(M):
+        if A[i][j] != B[i][j]:
+            print(-1)
+            exit()
+
+print(cnt)
