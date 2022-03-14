26ab9473b78ea4ddae528d3ad8a52c7cb6a281b9
unul09 <morning20404@gmail.com>
2022-03-14 22:54:32 +0900

    3009

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-# 1085
+# 3009
 
-x, y, w, h = map(int, input().split())
-dis = [x, y, w-x, h-y]
-print(min(dis))
+x = []
+y = []
+for _ in range(0, 3):
+    px, py = map(int, input().split())
+    x.append(px)
+    y.append(py)
+
+x.sort()
+y.sort()
+
+if x[0] == x[1]:
+    px = x[2]
+else:
+    px = x[0]
+
+if y[0] == y[1]:
+    py = y[2]
+else:
+    py = y[0]
+
+print(px, py)
