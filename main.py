3dfe8aeb70df77bff3ee6a530e1acbcb19730256
unul09 <morning20404@gmail.com>
2022-03-14 22:16:01 +0900

    2480

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# 2525
+# 2480
 
-A, B = map(int, input().split())
-C = int(input())
+dice = list(map(int, input().split()))
+dice.sort()
 
-H = A
-M = B+C
+if dice[0] == dice[1] and dice[1] == dice[2]:
+    total = 10000 + dice[0]*1000
+elif dice[0] == dice[1] or dice[1] == dice[2]:
+    total = 1000 + dice[1]*100
+else:
+    total = dice[2]*100
 
-while M >= 60:
-    M -= 60
-    H += 1
-
-    if H >= 24:
-        H -= 24
-
-print(H, M)
+print(total)
