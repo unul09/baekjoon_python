7ae443139b20ba026e77f4b09cef8cd8c4ee7904
unul09 <morning20404@gmail.com>
2022-03-14 22:41:48 +0900

    1085

--- main.py
+++ main.py
@@ -1,25 +1,5 @@
-# 9020
-import math
+# 1085
 
-
-def isPrime(n):
-    if n <= 3: return True
-    for i in range(2, math.floor(math.sqrt(n)) + 1):
-        if n % i == 0: return False
-    return True
-
-
-T = int(input())
-for _ in range(0, T):
-    n = int(input())
-    p1 = int(n / 2)
-    p2 = n - p1
-
-    while (not isPrime(p1)) or (not isPrime(p2)):
-        p1 -= 1
-        p2 += 1
-
-    print(p1, p2)
-
-
-
+x, y, w, h = map(int, input().split())
+dis = [x, y, w-x, h-y]
+print(min(dis))
