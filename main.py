d2a29a6d72aaf73366e4d93a3a1e740a49b7c02d
unul09 <morning20404@gmail.com>
2022-03-15 11:51:46 +0900

    10872

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-# 1002
-import math
+# 10872
 
-T = int(input())
+def factorial(n):
+    if n == 0: return 1
+    return factorial(n-1) * n
 
-for _ in range(0, T):
-    x1, y1, r1, x2, y2, r2 = map(int, input().split())
-    ans = 0
-    dis = math.sqrt((x1-x2)**2 + (y1-y2)**2)
-    if r2 > r1: r2, r1 = r1, r2
 
-    if dis == 0 and r1 == r2:
-        ans = -1
-    elif dis == r1+r2 or dis == abs(r1-r2):
-        ans = 1
-    elif abs(r1-r2) < dis < (r1+r2): # 이 조건에서 계속 틀린듯. 조건 명확히 설정할것
-        ans = 2
-    else:
-        ans = 0
-
-    print(ans)
+N = int(input())
+print(factorial(N))
