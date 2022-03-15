6aa148b4c1231b81baeeb5e79af73f4d296ffd9d
unul09 <morning20404@gmail.com>
2022-03-15 11:55:24 +0900

    10870

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-# 10872
+# 10870
 
-def factorial(n):
-    if n == 0: return 1
-    return factorial(n-1) * n
+def fibo(n):
+    if n == 0: return 0
+    if n == 1: return 1
+    return fibo(n-2) + fibo(n-1)
 
 
 N = int(input())
-print(factorial(N))
+print(fibo(N))
