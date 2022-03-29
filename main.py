ddd46980507a22b47668e0519dd0b92ac2f58b78
unul09 <morning20404@gmail.com>
2022-03-30 08:38:29 +0900

    1436

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-# 1018
+# 1436
 
-standard_chessboard = []
-for i in range(8):
-    if i % 2 == 0:
-        standard_chessboard.append(list('WBWBWBWB'))
+N = int(input())
+
+cnt = 0
+i = 666
+
+while True:
+    if '666' in str(i):
+        cnt += 1
+    if cnt == N:
+        print(i)
+        exit()
     else:
-        standard_chessboard.append(list('BWBWBWBW'))
-
-N, M = map(int, input().split())
-board = []
-for _ in range(N):
-    board.append(list(input()))
-
-color_min = 64
-for i in range(N-7):
-    for j in range(M-7):
-        cnt = 0
-        for a in range(8):
-            for b in range(8):
-                if board[i+a][j+b] != standard_chessboard[a][b]:
-                    cnt += 1
-        if cnt > 32: cnt = 64 - cnt
-        if color_min > cnt: color_min = cnt
-
-print(color_min)
+        i += 1
