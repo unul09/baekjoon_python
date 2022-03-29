7260cd036d2f6c41387f94fa9c983366deab2273
unul09 <morning20404@gmail.com>
2022-03-30 01:37:31 +0900

    2231

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-# 2798
+# 2231
 
-import itertools
+N = int(input())
 
-N, M = map(int, input().split())
-cards = list(map(int, input().split()))
+if N < 20:
+    min = 1
+else:
+    min = N - len(str(N)) * 9
 
-cards_3 = list(itertools.combinations(cards, 3))
-card_sums = list(sum(card_sum) for card_sum in cards_3)
 
-card_sums.sort()
-for i in range(len(card_sums)):
-    if card_sums[i] > M: # 초과하는 인덱스 바로 전의 값을 출력
-        if i == 0: # 첫번째부터 초과할 경우, 첫번째가 출력되도록
-            i += 1
-        print(card_sums[i-1])
-        exit()
-    if i == len(card_sums)-1: # 마지막까지 M보다 작을 경우
-        print(card_sums[i])
+for i in range(min, N + 1):
+    result = i + sum(list(map(int, str(i))))
+
+    if result == N:
+        print(i)
         exit()
 
+print(0)
 
 
